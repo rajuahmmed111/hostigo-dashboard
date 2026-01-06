9e28ac1e8dcca46c370d2923600b72354d517f44
Raju Ahmmed <ahraju2003@gmail.com>
2026-01-06 06:55:41 +0600

    report

--- src/redux/api/reports.js
+++ src/redux/api/reports.js
@@ -14,6 +14,7 @@ export const reportsApi = baseApi.injectEndpoints({
       }),
       providesTags: ["Reports"],
     }),
+    
     // delete report
     deleteReport: builder.mutation({
       query: (id) => ({
