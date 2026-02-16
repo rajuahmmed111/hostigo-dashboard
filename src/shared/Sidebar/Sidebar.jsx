e95d23d43258d15498accded7fc0e8e4705f8965
Raju Ahmmed <ahraju2003@gmail.com>
2026-02-17 01:04:26 +0600

    comment  chats bar

--- src/shared/Sidebar/Sidebar.jsx
+++ src/shared/Sidebar/Sidebar.jsx
@@ -8,7 +8,7 @@ import { IoCloseSharp, IoLogOutOutline } from "react-icons/io5";
 import { MdAdminPanelSettings } from "react-icons/md";
 
 import { BsCurrencyDollar, BsCreditCard, BsCheckCircle } from "react-icons/bs";
-import { BiChat } from "react-icons/bi";
+// import { BiChat } from "react-icons/bi";
 import { logout } from "../../redux/features/auth/authSlice";
 import { useLogoutMutation } from "../../redux/api/authApi";
 import { message } from "antd";
@@ -172,7 +172,7 @@ const Sidebar = ({ isOpen, toggleSidebar }) => {
           </Link>
 
           {/* chats */}
-          <Link to="/chat" onClick={handleLinkClick}>
+          {/* <Link to="/chat" onClick={handleLinkClick}>
             <li
               className={`flex items-center gap-2 mt-2 cursor-pointer transition-all duration-300 ease-in-out ${
                 isActive("/chat")
@@ -183,7 +183,7 @@ const Sidebar = ({ isOpen, toggleSidebar }) => {
               <BiChat className="w-5 h-5" />
               <p className="text-lg font-semibold">Chat</p>
             </li>
-          </Link>
+          </Link> */}
 
           {/* Create Admin */}
           <Link to="/create-admin">
